2dedd5f6fe1ec6e51279c66a7c3fd4d8d1ad5b70
Samartha Hegde <samarthhegdekalgar@gmail.com>
2019-09-18 09:46:26 +0530

    Whats going on?

---
+++ print-formattin.py
@@ -0,0 +1,14 @@
+name = "Samarth"
+age = 23
+salary = 10000000000
+hair_color = "black"
+height = 5.6
+weight = 65
+education_degree = "B.E"
+print(f"My name is {name}")
+print(f"My age is {age}")
+print(f"My salary is {salary}")
+print(f"My hair color is {hair_color}")
+print(f"My height is {height}")
+print(f"My weight is {weight}")
+print(f"My education is {education_degree}")
