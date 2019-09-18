089324d3dd29bacef7b03d5fdad633e4e92d5071
Samartha Hegde <samarthhegdekalgar@gmail.com>
2019-09-18 10:06:50 +0530

    Crazy coding practice

---
+++ separaters1.py
@@ -0,0 +1,8 @@
+name = "Samarth"
+age = 23
+salary = 10000000000
+hair_color = "black"
+height = 5.6
+weight = 65
+education_degree = "B.E"
+print(name,age,salary,hair_color,height,weight,education_degree, sep=',')
