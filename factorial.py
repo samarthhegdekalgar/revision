e46ac7e4570d921416e7190e9706fbf0cfd687dd
Samartha Hegde <samarthhegdekalgar@gmail.com>
2019-09-18 11:32:06 +0530

    updated with recursion

--- factorial.py
+++ factorial.py
@@ -5,6 +5,15 @@ def get_factorial(number):
     return result
 
 
+def get_factorial_by_recursion(number):
+    if number == 1:
+        return number
+    else:
+        return number * get_factorial_by_recursion(number - 1)
+
+
 user_input = int(input("Enter the number:\t"))
 fact_result = get_factorial(user_input)
+print(f"Factorial is : {fact_result}")
+fact_result = get_factorial_by_recursion(user_input)
 print(f"Factorial is : {fact_result}")
